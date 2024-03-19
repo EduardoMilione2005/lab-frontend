5d4c2b7c565d70655ece94868d947028865df690
Aluno UniAcademia <alunouni@labcesac.net>
2024-03-18 22:10:16 -0300

    Adicionando Hello World, Vitor

--- 03-estrutura/src/App.js
+++ 03-estrutura/src/App.js
@@ -1,3 +1,3 @@
 export default function App() {
-    return <h1>Vitor Coimbra, o gênio da programação</h1>
+    return <h1>Hello World, Vitor Coimbra</h1>
 }
--- react-vitor/src/app/page.js
+++ react-vitor/src/app/page.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Vitor</h1>;
+  return <h1>Olá Mundo, Vitor Coimbra</h1>;
 }
