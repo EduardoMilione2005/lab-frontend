4c2f1c2c015ac3b59a731e3980afb982899f9397
EduardoMilione2005 <eduardo.milione8@gmail.com>
2024-03-20 21:46:38 -0300

    Subtítulo e parágrafo em outra branch

--- 03-estrutura/src/App.js
+++ 03-estrutura/src/App.js
@@ -1,5 +1,9 @@
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <h2>Leo e Breno</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aperiam fuga ratione facilis repellat architecto, deleniti assumenda iure fugit magni harum, cupiditate, voluptatibus ab officiis consequuntur pariatur. Dicta, at odio.</p>
+    </div>
   );
 }
